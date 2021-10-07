a33591aecd15fc2cbe660e86d5d9c3554c169fee
Eduardo Lins <eduardolimalins@gmail.com>
2021-10-06 21:10:38 -0300

    Resending corrected files

--- a-estrela.py
+++ a-estrela.py
@@ -1,7 +1,7 @@
 #destination: MAR
 #TO-DO: Run 'pip install opencv-python' on CMD
 import cv2
-image = cv2.imread('grafo_franca(pins).png')
+image = cv2.imread('.\grafo_franca(pins).png')
 
 #list of adjacences (COORDINATES)
 list_adj = [[["CAL","LIL"] , [432,28,508,56]] # [["A","B"], [Xa, Ya, Xb, Yb]]
@@ -33,9 +33,7 @@ list_adj = [[["CAL","LIL"] , [432,28,508,56]] # [["A","B"], [Xa, Ya, Xb, Yb]]
 ,[["REN", "ORL"] , [213,285,433,297]]
 ,[["REN", "CAE"] , [213,285,294,192]]
 ,[["CAE", "PAR"] , [294,192,462,218]]
-,[["CAE", "LEH"] , [294,192,331,149]]
-,[["LEH", "PAR"] , [331,149,462,218]]
-,[["LEH", "AMI"] , [331,149,455,105]]
+,[["CAE", "AMI"] , [294,192,455,105]]
 ,[["AMI", "PAR"] , [455,105,462,218]]
 ,[["PAR", "ORL"] , [462,218,433,297]]
 ,[["ORL", "BOU"] , [433,297,459,377]]
@@ -47,60 +45,68 @@ list_adj = [[["CAL","LIL"] , [432,28,508,56]] # [["A","B"], [Xa, Ya, Xb, Yb]]
 
 CAL =  {"name":"CAL", "adj":["AMI", "LIL"], "dist":[122.14, 91.27]} #CAL
 LIL =  {"name":"LIL", "adj":["CAL", "AMI", "PAR", "MET"], "dist":[91.27,95.68,205.56,280.15]}#LIL
-AMI =  {"name":"AMI", "adj":["CAL", "LIL", "PAR", "LEH"], "dist":[122.14,95.68,117.89,162.39]}#AMI
-Le_Havre =  {"name":"LEH", "adj":["AMI", "PAR", "CAE"], "dist":[162.39,178.41,49.56]}#LEH
-CAE =  {"name":"CAE", "adj":["LEH", "PAR", "REN"], "dist":[49.56,201.85,152.58]}#CAE
+AMI =  {"name":"AMI", "adj":["CAL", "LIL", "PAR", "CAE"], "dist":[122.14,95.68,117.89,207.91]}#AMI
+CAE =  {"name":"CAE", "adj":["AMI", "PAR", "REN"], "dist":[207.91,201.85,152.58]}#CAE
 MET =  {"name":"MET", "adj":["LIL", "PAR", "DIJ", "STR"], "dist":[280.15,279.86,214.9,129.82]}#MET
-PAR = {"name":"PAR", "adj":["AMI", "LIL", "MET", "ORL", "CAE", "LEH"], "dist":[117.89, 205.56, 279.86,107.5,201.85,178.41]}#PAR
+PAR = {"name":"PAR", "adj":["AMI", "LIL", "MET", "ORL", "CAE"], "dist":[117.89, 205.56, 279.86,107.5,201.85]}#PAR
 REN =  {"name":"REN", "adj":["CAE", "ORL", "NAN"], "dist":[152.58, 268.94, 99.65]}#REN
 ORL =  {"name":"ORL", "adj":["PAR", "REN", "BOU"], "dist":[107.5, 268.94,98.28]}#ORL
 STR =  {"name":"STR", "adj":["MET", "DIJ", "LYO"], "dist":[129.82,243.05,381.75]}#STR
 NAN =  {"name":"NAN", "adj":["REN", "LAR"], "dist":[99.65,121.32]}#NAN
 BOU = {"name":"BOU", "adj":["ORL", "DIJ", "LYO", "LAR"], "dist":[98.28, 201.93, 237.98,288.32]}#BOU
 DIJ = {"name":"DIJ", "adj":["MET", "STR", "LYO", "BOU"], "dist":[214.9,243.05,173.84,201.93]}#DIJ
-La_Rochelle = {"name":"LAR", "adj":["NAN", "BOR"], "dist":[121.32,152.46]}#LAR
+LAR = {"name":"LAR", "adj":["NAN", "BOR"], "dist":[121.32,152.46]}#LAR
 BOR = {"name":"BOR", "adj":["LAR", "STE", "TOU"], "dist":[152.46,395.48,211.43]}#BOR
-St_Etienne = {"name":"STE", "adj":["BOR", "TOU", "LYO"], "dist":[395.48,314.15,49.67]}#STE
+STE = {"name":"STE", "adj":["BOR", "TOU", "LYO"], "dist":[395.48,314.15,49.67]}#STE
 LYO = {"name":"LYO", "adj":["BOU", "DIJ", "STR", "NIC", "AVI", "STE"], "dist":[237.98,173.84,381.75,297.23,201.71,49.67]} #LYO
 TOU = {"name":"TOU", "adj":["BOR", "STE", "AVI"], "dist":[211.43, 314.15,274.17]}#TOU
 AVI = {"name":"AVI", "adj":["TOU", "LYO", "NIC", "MAR"], "dist":[274.17,201.71,197.44,81.6]}#AVI
 NIC = {"name":"NIC", "adj":["MAR", "AVI", "LYO"], "dist":[159.02, 197.44, 297.23]}#NIC
 MAR = {"name":"MAR", "adj":["AVI", "NIC"], "dist":[81.6, 159.02]}#MAR
 
-nodes = {"CAL": CAL, "LIL": LIL, "AMI": AMI, "LEH": Le_Havre,
+nodes = {"CAL": CAL, "LIL": LIL, "AMI": AMI,
 "CAE": CAE, "MET": MET, "PAR": PAR, "REN": REN, "ORL": ORL, "STR":STR, "NAN":NAN,
-"BOU":BOU, "DIJ": DIJ, "LAR":La_Rochelle, "BOR":BOR, "STE":St_Etienne,"LYO": LYO, "TOU":TOU,
+"BOU":BOU, "DIJ": DIJ, "LAR":LAR, "BOR":BOR, "STE":STE,"LYO": LYO, "TOU":TOU,
 "AVI":AVI,"NIC":NIC,"MAR":MAR}
 
-heuristic = {"CAL":890.59, "LIL":832.45, "AMI":768.69, "LEH":796.98, "CAE":787.53,"MET":649.03,"PAR":659.4,"REN":764.26,
+heuristic = {"CAL":890.59, "LIL":832.45, "AMI":768.69, "CAE":787.53,"MET":649.03,"PAR":659.4,"REN":764.26,
 "ORL":577.16,"STR":613.55,"NAN":696.16,"BOU":475.15,"DIJ":441.84,"LAR":615.94,"BOR":501.12,"STE":251.17,"LYO":274.93,
 "TOU":318.32,"AVI":81.6,"NIC":159.02,"MAR":0} #distance from each node to the destination in straight line
 curr_adj = [] #adjacent nodes of the currrent node
 f_ns = [] #list of the evaluation functions for each adjacent of the current node
 dist_acc = 0 #distance accumulated from the first node to the current
 path = [] #list of the cities that compound the shortest path
+to_visit = [] #list of the nodes to visit in each iteration
 nodes_adj = [] #pair of nodes that compound an adjacence
 
 stop = False
-origin = "CAL"
-curr = nodes[origin] #the first node
+
+origin = input("Type the origin: ") #CAL, LIL, ...
+dest = nodes["MAR"] #destination
+
+curr = nodes[origin] #the first node (and the current node for each iteration)
 path.append(origin)
 curr_adj = curr["adj"]
-dest = nodes["MAR"] #destination
 nodes_adj.append(origin) 
 
-print("Origin: " + origin)
-print("********************************")
+print("\nOrigin: " + origin)
+print("********************************\n")
+if(curr["name"] == "MAR"):
+    print("You typed the destination")
+    stop = True
 while not stop:
     i = 0
     for node in curr_adj:
-         f_ns.append(heuristic[node]+(curr["dist"][i]+dist_acc))
-         i += 1
-    for node in curr_adj:
+        if(not(node in path)): #if the node was not visited
+            f_ns.append(heuristic[node]+(curr["dist"][i]+dist_acc))
+            to_visit.append(node)
+        i += 1
+    for node in to_visit:
          if(node == dest["name"]):
              stop = True
-             print("The shorter path was found")
-    print(curr_adj)
+             print("**The destination was found**")
+             break
+    print(to_visit)
     print(f_ns)
     aux = f_ns[0]
     index_less = 0 #the index of the current less value of f_ns
@@ -110,8 +116,8 @@ while not stop:
             aux = value
             index_less = i
         i += 1
-    dist_acc += curr["dist"][index_less]
-    curr = nodes[curr_adj[index_less]]
+    dist_acc += curr["dist"][curr["adj"].index(to_visit[index_less])]
+    curr = nodes[to_visit[index_less]]
     nodes_adj.append(curr["name"])
     for pair in list_adj: 
         if((nodes_adj[0] in pair[0]) and (nodes_adj[1] in pair[0])):
@@ -119,6 +125,7 @@ while not stop:
     nodes_adj.pop(0)
     curr_adj = curr["adj"]
     f_ns.clear()
+    to_visit.clear()
     path.append(curr["name"])
     print("f(n) = " + str(aux) + " -> " + curr["name"])
     print("---------------------------------")
